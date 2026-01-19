3aa5070fb1d8205701fd7957bddd4afdca52974b
jeffersonLucass <jeffersonlucas810@gmail.com>
2026-01-19 11:00:55 -0300

    feat:Cria entidade OrdemServico com seus devidos atributos

---
+++ src/main/java/io/github/jeffersonLucass/OrdemdeServico_api/Enums/StatusOrdemServico.java
@@ -0,0 +1,8 @@
+package io.github.jeffersonLucass.OrdemdeServico_api.Enums;
+
+public enum StatusOrdemServico {
+    ABERTA,
+    EM_ANDAMENTO,
+    FINALIZADA,
+    CANCELADA
+}
--- src/main/java/io/github/jeffersonLucass/OrdemdeServico_api/entity/OrdemServico.java
+++ src/main/java/io/github/jeffersonLucass/OrdemdeServico_api/entity/OrdemServico.java
@@ -1,11 +1,16 @@
 package io.github.jeffersonLucass.OrdemdeServico_api.entity;
 //SOLICITACAO DE SERVICO FEITA POR UM CLIENTE
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import io.github.jeffersonLucass.OrdemdeServico_api.Enums.StatusOrdemServico;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Entity
 @Table(name = "OrdemServico",schema = "public")
@@ -13,6 +18,37 @@ import lombok.ToString;
 @ToString
 public class OrdemServico {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NotNull(message = "Cliente é obrigatório")
+    @ManyToOne
+    @JoinColumn(name = "cliente_id", nullable = false)
+    private Cliente cliente;
+
+    @Column(nullable = false)
+    private String descricao;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private StatusOrdemServico status;
+
+    private BigDecimal valor;
+
+    @Column(nullable = false,updatable = false)
+    private LocalDateTime dataAbertura;
+
+    private LocalDateTime dataConclusao;
+
+    //OS sempre é criada Aberta
+
+    @PrePersist
+    protected void onCreate() {
+        this.status = StatusOrdemServico.ABERTA;
+        this.dataAbertura = LocalDateTime.now();
+    }
 
 
 
