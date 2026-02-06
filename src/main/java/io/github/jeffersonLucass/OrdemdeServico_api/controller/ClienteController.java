0ea51d5d9d553ec5940a7d7f98a38abbafa5dfbc
jeffersonLucass <jeffersonlucas810@gmail.com>
2026-02-06 10:21:55 -0300

    feat: adcrimplementação do clienteController com metodo HTTP post

--- src/main/java/io/github/jeffersonLucass/OrdemdeServico_api/controller/ClienteController.java
+++ src/main/java/io/github/jeffersonLucass/OrdemdeServico_api/controller/ClienteController.java
@@ -1,9 +1,35 @@
 package io.github.jeffersonLucass.OrdemdeServico_api.controller;
 
+import io.github.jeffersonLucass.OrdemdeServico_api.entity.Cliente;
+import io.github.jeffersonLucass.OrdemdeServico_api.repository.ClienteRepository;
+import io.github.jeffersonLucass.OrdemdeServico_api.service.ClienteService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/cliente")
 public class ClienteController {
+
+    private final ClienteRepository clienteRepository;
+    private final ClienteService clienteService;
+
+    public ClienteController(ClienteRepository clienteRepository,ClienteService clienteService) {
+        this.clienteRepository = clienteRepository;
+        this.clienteService = clienteService;
+    }
+
+    @PostMapping
+    public Cliente criarCliente(@RequestBody Cliente cliente){
+        System.out.println("Cliente criado com sucesso " + cliente);
+
+        return clienteService.criar(cliente);
+    }
+
+
+
+
+
+
 }
